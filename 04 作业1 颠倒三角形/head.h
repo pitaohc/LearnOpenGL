07b92f47e5d69520216fd4ade3bc05fbcc2248e5
pitaohc <pitaohc@qq.com>
2022-08-06 00:28:22 +0800

    1. 增加第三方库目录 2. 修改项目属性：头文件、库 3. 创建04 作业1 4. 使用宏区分控制台程序和窗口程序

---
+++ 04 作业1 颠倒三角形/head.h
@@ -0,0 +1,16 @@
+#pragma once
+#ifndef HEAD_H
+#define HEAD_H
+
+#include<glad/glad.h>
+#include<glfw3.h>
+#include<iostream>
+#include<string>
+#include<cmath>
+#include<ctime>
+#include<fstream>
+#include<sstream>
+#include<Windows.h>
+extern unsigned int HEIGHT, WIDTH; //高和宽
+extern std::string TITLE; //窗口标题
+#endif
